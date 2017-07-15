5406fa2458bf86a5a878efe9645fc938ec090aca
Andrey Aratov <madingame@yandex.ru>
2017-07-15 21:15:03 +0300

    map popup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-$(document).ready(function() {
-    console.log('Alive!!!');
+document.addEventListener('DOMContentLoaded', function() {
+    showPopup();
+    closePopup();
 });
+
+function showPopup() {
+    var mapTriggerBtn = document.querySelector('.js-show-map');
+    var mapPopup = document.querySelector('.popup--map');
+    if(mapPopup && mapTriggerBtn) {
+        mapTriggerBtn.addEventListener('click', function() {
+            mapPopup.classList.add('popup--active');
+        });
+    }
+}
+
+
+function closePopup() {
+    var closeBtn = document.querySelector('.popup__close');
+    if(closeBtn) {
+        closeBtn.addEventListener('click', function() {
+            this.parentElement.classList.remove('popup--active');
+        });
+    }
+}
